35ec48dd601ff6636a0ff8ae9c989e37390f6986
江波俊介 <enamishunsuke@XC230B0140.local>
2023-10-04 10:42:27 +0900

    delete

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { GameView } from './view';
-import { GameModel } from './model';
-import { GameController } from './controller';
-
-const view = new GameView();
-const model = new GameModel();
-const controller = new GameController(view, model);
-
-controller.play();
